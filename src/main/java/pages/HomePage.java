5656058229862569bf1dc5a6ec8113130b420192
Sam <68948584+sambhajiaute@users.noreply.github.com>
2022-07-16 21:53:31 +0530

    commit2

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
-	
+	//adding comment
 	
 	@FindBy(xpath="//*[@id='welcome']")
 	private WebElement userMenu;
